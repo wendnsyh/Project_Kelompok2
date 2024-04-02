925bd65deea54f5844f1e41283465639abb75bba
wendnsyh <wendy.martadiandsyah75@gmail.com>
2024-04-02 11:31:40 +0700

    Menambahkan view  baru pada side bar

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -3,6 +3,7 @@
     <!-- Page Wrapper -->
     <div id="wrapper">
 
+      
         <!-- Sidebar -->
         <ul class="navbar-nav bg-gradient-danger sidebar sidebar-dark accordion" id="accordionSidebar">
 
@@ -19,7 +20,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="<?php echo base_url('admin/dashboard')?>">
+                <a class="nav-link" href="<?php echo base_url('admin/dashboard') ?>">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -27,41 +28,35 @@
             <!-- Divider -->
             <hr class="sidebar-divider">
             <li class="nav-item active">
-                <a class="nav-link" href="<?php echo base_url('penduduk/tampil')?>">
+                <a class="nav-link" href="<?php echo base_url('penduduk/tampil') ?>">
                     <i class="fas faw-fw fa-address-card"></i>
                     <span>Data Penduduk</span></a>
             </li>
 
-            <!-- Divider -->
             <hr class="sidebar-divider">
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo base_url('Kelahiran/tampil') ?>">
+                    <i class="fas faw-fw fa-book"></i>
+                    <span>Data Kelahiran</span></a>
+            </li>
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Addons
-            </div>
+            <hr class="sidebar-divider">
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo base_url('kematian/tampil') ?>">
+                    <i class="fas faw-fw fa-history"></i>
+                    <span>Data Kematian</span></a>
+            </li>
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
-                </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Login Screens:</h6>
-                        <a class="collapse-item" href="login.html">Login</a>
-                        <a class="collapse-item" href="register.html">Register</a>
-                        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                        <div class="collapse-divider"></div>
-                        <h6 class="collapse-header">Other Pages:</h6>
-                        <a class="collapse-item" href="404.html">404 Page</a>
-                        <a class="collapse-item" href="blank.html">Blank Page</a>
-                    </div>
-                </div>
+            <hr class="sidebar-divider">
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo base_url('pindah/tampil') ?>">
+                    <i class="fas faw-fw fa-address-card"></i>
+                    <span>Data Pindah</span></a>
             </li>
 
             <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
+            <hr class="sidebar-divider">
+            <li class="nav-item active">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-envelope"></i>
                     <span>Layanan Surat</span>
@@ -95,34 +90,35 @@
             <!-- Divider -->
             <hr class="sidebar-divider">
 
-        <!-- Nav Item - Charts -->
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url('beranda')?>">
-                <i class="fas fa-fw fa-home"></i>
-                <span>Beranda</span></a>
-        </li>
+            <!-- Nav Item - Charts -->
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo base_url('beranda') ?>">
+                    <i class="fas fa-fw fa-home"></i>
+                    <span>Beranda</span></a>
+            </li>
 
-        <!-- Nav Item - Tables -->
-        <li class="nav-item">
-            <a class="nav-link" href="tables.html">
-                <i class="fas fa-fw fa-table"></i>
-                <span>Tables</span></a>
-        </li>
+            <hr class="sidebar-divider">
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo base_url('pengaturan') ?>">
+                    <i class="fas fa-cog"></i>
+                    <span>Pengaturan</span></a>
+            </li>
 
-        <!-- Divider -->
-        <hr class="sidebar-divider d-none d-md-block">
+            <hr class="sidebar-divider">
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo base_url('pengaturan') ?>">
+                    <i class="fas fa-power-off"></i>
+                    <span>Logout</span></a>
+            </li>
 
-        <!-- Sidebar Toggler (Sidebar) -->
-        <div class="text-center d-none d-md-inline">
-            <button class="rounded-circle border-0" id="sidebarToggle"></button>
-        </div>
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
 
-        <!-- Sidebar Message -->
-        <div class="sidebar-card d-none d-lg-flex">
-            <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
-            <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-            <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
-        </div>
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
 
         </ul>
         <!-- End of Sidebar -->
